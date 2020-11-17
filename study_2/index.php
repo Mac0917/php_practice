ff461447d528a2f3fd5d67c2f124fdb7fba48b3f
T.M <62660992+Mac0917@users.noreply.github.com>
2020-11-17 10:51:56 +0900

    Update index.php

--- study_2/index.php
+++ study_2/index.php
@@ -4,7 +4,7 @@
 <html>
 <head>
   <meta charset="utf-8">
-  <title>Café Progate</title>
+  <title>php</title>
   <link rel="stylesheet" type="text/css" href="stylesheet.css">
   <link href='https://fonts.googleapis.com/css?family=Pacifico|Lato' rel='stylesheet' type='text/css'>
 </head>
